95a98b1e262d804a3b3577d3e0c1745cec56c833
aanyamarija <124683108+aanyamarija@users.noreply.github.com>
2023-02-28 20:26:38 +0100

    Add files via upload
    First JS home work

---
+++ JS1/script.js
@@ -0,0 +1,103 @@
+//console.log("Hello World")
+
+//let myName = "Arsen"
+//let age = 24
+
+/* 
+
+//let degree = 10
+console.log(degree)
+degree = 15 
+console.log(degree)
+
+const number = 100
+console.log(number)
+number = 200
+console.log(number)
+
+// однострочный коментарий 
+/* многострочный коментарий */
+/*
+myName = "Arsen"
+console.log(myName)
+
+ const myAge = 24*/
+
+ //const myName = prompt( "Введите ваше имя")
+ //console.log(myName)
+
+ /*
+ const num1 = 10
+ const num2 = 20
+
+ const sum = num1 + num2 //30 
+ const sub = num2 - num1 // 10 subsctract - вычитание
+const mult = num1 * num2 // 200 mutiplication - произведение
+const div = num2 / num1 // 2 divide - деление
+const mod = num2 % num1 // 20/10 =2, 0 в остатке, 0. % - остаток от деления 
+// 11/3 = 3 2/3 - 2 в остатке 11 % 3 - 2 */
+
+
+ //const num = prompt("Введите число")
+// console.log(num * 100)
+
+//const fullName = "Arsen" + "Isupov"
+
+/*const firstName = "Arsen"
+const surname = "Isupov"
+const fullName = firstName + surname
+console.log(fullName)*/
+
+//const myName =  prompt("Meня зовут {имя пользователя}")
+//console.log(myName)
+
+//const name = prompt ("Введите имя")
+//console.log(" Меня зовут" + name)
+
+/*const price = 700
+if(price > 1500) {
+    console.log("скидка 15%")
+}
+else if (price > 1000) {
+    console.log("Скидка 10%")
+}
+else {
+    console.log( "Скидки нет")
+} */
+
+/*const num = prompt("Введите число")
+
+if(num > 20 ) {
+    console.log("Больше")
+}
+else if (num == 20) {
+    console.log("Равно")
+}
+if  (num < 20) {
+    console.log("меньше")
+}*/
+
+//HOME WORK 
+
+/*const num = prompt("Введите число")
+console.log(num/100*10)*/
+
+/*const num1 = prompt("Первое число")
+const num2 = prompt("Второе число")
+if ( num1 < num2) {
+console.log(num1)
+}
+else if ( num2 < num1) {
+    console.log(num2)
+} */
+
+/*const num = prompt("Введите число")
+if (num < 100) {
+    console.log("Число меньше 100")
+}
+if (num > 100) {
+    console.log("Число больше 100")
+}
+if (num == 100) {
+    console.log("Число равно 100")
+} */
